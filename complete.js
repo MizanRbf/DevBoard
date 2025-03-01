54bfe877d8da8a07a44108cf055c69f13981c24b
MizanRbf <mizanrbf@gmail.com>
2025-03-01 15:48:10 +0600

    taskassigned & navtask done

---
+++ complete.js
@@ -0,0 +1,16 @@
+const completeButton = document.querySelectorAll('.complete');
+const taskNumber = document.getElementById('task-number');
+const navCheckBox = document.getElementById('nav-checkbox');
+console.log(taskNumber);
+for(let i = 0; i < completeButton.length; i++){
+  let completed = completeButton[i];
+  completed.addEventListener('click',function(){
+    alert('Board Updated Successfully');
+  let currentTaskNumber = parseInt(taskNumber.innerText);
+  currentTaskNumber--;
+  taskNumber.innerText = currentTaskNumber;
+  let currentNavCheckBox = parseInt(navCheckBox.innerText);
+   currentNavCheckBox++;
+   navCheckBox.innerText = currentNavCheckBox;
+  })
+}
